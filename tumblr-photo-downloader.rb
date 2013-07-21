aa918891577cfaef658499796bcffe8045361922
Igor Sinkovec <igor.sinkovec@gmail.com>
2013-07-22 01:17:03 +0200

    Added support for full backups with max image limit. Added more status reporting. Removed loop control bug at the end.

--- tumblr-photo-downloader.rb
+++ tumblr-photo-downloader.rb
@@ -2,14 +2,29 @@ require 'rubygems'
 require 'bundler'
 Bundler.require
 
+def job_done()
+  # print status and exit
+  puts "Skipped #{$already_had} existing photos."
+  puts "Downloaded #{$new_images} new photos."
+  if $image_limit
+    puts $image_limit > $new_images ? "Failed to download #{$image_limit - $new_images} photos." : "All requested photos have been downloaded."
+  end
+  puts "Done."
+  exit
+end
+
 site = ARGV[0]
 directory = ARGV[1] ? ARGV[1] : site
+fullbackup = ARGV[2] ? "fullbackup" == ARGV[2] : false
+$image_limit = ARGV[3] ? ARGV[3].to_i : 0
 
-if site.nil? || site.empty?
+if ARGV.empty?
   puts
-  puts "Usage: #{File.basename(__FILE__)} URL [directory to save in]"
+  puts "Usage: #{File.basename(__FILE__)} URL [directory to save in] [fullbackup] [image limit]"
   puts "eg. #{File.basename(__FILE__)} jamiew.tumblr.com"
   puts "eg. #{File.basename(__FILE__)} jamiew.tumblr.com ~/pictures/jamiew-tumblr-images/"
+  puts "eg. #{File.basename(__FILE__)} jamiew.tumblr.com ~/pictures/jamiew-tumblr-images/ fullbackup"
+  puts "eg. #{File.basename(__FILE__)} jamiew.tumblr.com ~/pictures/jamiew-tumblr-images/ fullbackup 400"
   puts
   exit 1
 end
@@ -21,6 +36,8 @@ FileUtils.mkdir_p(directory)
 
 num = 50
 start = 0
+$new_images = 0
+$already_had = 0
 
 loop do
   url = "http://#{site}/api/read?type=photo&num=#{num}&start=#{start}"
@@ -35,9 +52,10 @@ loop do
   doc = Nokogiri::XML.parse(page.body)
 
   images = (doc/'post photo-url').select{|x| x if x['max-width'].to_i == 1280 }
+  puts "Found #{images.count} photos (page size = #{num})"
   image_urls = images.map {|x| x.content }
 
-  already_had = 0
+  done_images = 0
 
   image_urls.each_slice(concurrency).each do |group|
     threads = []
@@ -47,7 +65,7 @@ loop do
           filename = url.split("/").last
           if File.exists?("#{directory}/#{filename}")
             puts "Already have #{url}\n"
-            already_had += 1
+            $already_had += 1
           else
             begin
               file = Mechanize.new.get(url)
@@ -58,26 +76,32 @@ loop do
             end
             puts "Saving photo #{filename}\n"
             file.save_as("#{directory}/#{filename}")
+            $new_images += 1
+
+            if $new_images == $image_limit
+              # reached image limit
+              job_done()
+            end
           end
 
+          done_images += 1
+
         rescue Mechanize::ResponseCodeError
-          puts "Error getting file, #{$!}"
+          puts "Error getting file, #{$!}\n"
         end
       }
     end
     threads.each{|t| t.join }
   end
 
-  puts "#{images.count} images found (num=#{num})"
-
-  if images.count < num
-    puts "Our work here is done"
-    break
-  elsif already_had == num
-    puts "Had already downloaded the last #{already_had} of #{num} most recent images - done."
-    break
+  if images.count == done_images
+    # touched (downloaded or skipped) all found images
+    if fullbackup && images.count > 0
+      # doing full backup and not yet reached the end of tumblr archive
+      start += num
+      # continue the main loop
     else
-    start += num
+      job_done() # exits script
+    end
   end
-
 end
